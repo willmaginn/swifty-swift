eb3f55b1e6114f6d46713f729f5ca2f7649ed264
Will Maginn <will@thetinypixel.com>
2016-01-29 00:04:21 +0000

    Finished initial exploring of basic data types

--- 1. Exploring.playground/Contents.swift
+++ 1. Exploring.playground/Contents.swift
@@ -15,7 +15,12 @@ var y = 10 // mutable
 //x = 5 // Error
 
 /*:
+## Exploring basic data types
+*/
+
+/*:
 ## Arrays
+Collection of ordered items
 */
 
 var implictArray = ["one", "two", "three"]
@@ -38,3 +43,78 @@ var joinArray = two + oneTwo
 
 var range = 3...4
 var range2 = 0..<2
+
+/*:
+## Sets
+Collection of unordered items
+*/
+
+var mySet: Set<Int> = [1, 2, 3, 4]
+
+mySet.sort(<)
+mySet.insert(5)
+mySet.underestimateCount()
+mySet.count
+mySet.indexOf(2)
+
+/*:
+## Dictionary
+Collection of key value items
+*/
+
+var myDict: Dictionary<Int, String> = [1:"one", 2:"two", 3:"three"]
+
+myDict[1]
+myDict.updateValue("four", forKey: 4)
+myDict
+myDict.updateValue("FOUR", forKey: 4)
+myDict.updateValue("", forKey: 4)
+myDict
+myDict.removeValueForKey(4)
+myDict
+
+/*:
+## String
+Working with strings and chracter
+*/
+
+var name: String = "Will Maginn"
+
+let reverseName = String(name.characters.reverse())
+
+for char in name.characters {
+    print("Character: \(char)")
+}
+
+let firstName = "Will"
+let surname = "Maginn"
+
+firstName + " " + surname
+var wholeName = firstName
+
+wholeName.append(Character(" "))
+wholeName.appendContentsOf(surname)
+
+let from = wholeName.startIndex
+let to = wholeName.startIndex.advancedBy(3)
+wholeName[from...to]
+
+/*:
+## Int
+Working with Ints
+*/
+
+let luckyNumber = 14
+let luckyInt16: Int16 = 14 // Also UInt for storing greater non-negative values
+let luckyInt64: Int64 = 14
+
+luckyNumber == Int(luckyInt16)
+luckyNumber == Int(luckyInt64)
+Int64(luckyNumber) == luckyInt64
+
+// Favor Double over Float
+
+let luckyFloat: Float = 14.99999
+let luckyDouble: Double = 14.9999999199999
+
+
